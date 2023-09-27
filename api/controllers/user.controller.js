9ebd5227932da19ebbd9b4dda13053f7bf15dde3
Vignesh <vigneshpamu2002@gmail.com>
2023-09-28 00:12:05 +0530

    Create get user listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model.js'
+import Listing from '../models/listing.model.js'
 import { errorHandler } from '../utils/error.js'
 import bcryptjs from 'bcryptjs'
 export const test = (req, res) => {
@@ -45,5 +46,18 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json('User Deleted Successfully')
   } catch (error) {
     next(error)
+  }
+}
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id })
+      res.status(200).json(listings)
+    } catch (error) {
+      next(error)
+    }
+  } else {
+    return next(errorHandler(401, 'You can only update your own account!'))
   }
 }
