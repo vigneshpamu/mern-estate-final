c6875442b5196403be57f0ed76ffcd487053ee99
Vignesh <vigneshpamu2002@gmail.com>
2023-09-27 23:00:10 +0530

    Complete Listing Page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -20,6 +20,8 @@ import {
   signOutUserFailure,
 } from '../redux/user/userSlice'
 import { app } from '../firebase'
+import { Link } from 'react-router-dom'
+
 const Profile = () => {
   const fileRef = useRef(null)
   const { currentUser, loading, error } = useSelector((state) => state.user)
@@ -178,6 +180,12 @@ const Profile = () => {
         >
           {loading ? 'Loading' : 'Update'}
         </button>
+        <Link
+          to="/create-listing"
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
