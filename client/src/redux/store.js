7baea4f47d41b010849f4e033fa564433d8543e8
Vignesh <vigneshpamu2002@gmail.com>
2023-09-27 18:58:20 +0530

    Update Header and Make Profile Page to Private

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -16,9 +16,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    user: persistedReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
