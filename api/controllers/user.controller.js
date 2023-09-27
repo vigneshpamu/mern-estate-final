45105b480601ccb7829ff391687cd476cbc0366d
Vignesh <vigneshpamu2002@gmail.com>
2023-09-27 22:01:06 +0530

    Added Delete User Functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -8,8 +8,6 @@ export const test = (req, res) => {
 }
 
 export const updateUser = async (req, res, next) => {
-  console.log(req.user.id)
-  console.log(req.params.id)
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, 'You can only update your own account!'))
   try {
@@ -33,6 +31,18 @@ export const updateUser = async (req, res, next) => {
     const { password, ...rest } = updatedUser._doc
 
     res.status(200).json(rest)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only update your own account!'))
+  try {
+    await User.findByIdAndDelete(req.params.id)
+    res.clearCookie('access_token')
+    res.status(200).json('User Deleted Successfully')
   } catch (error) {
     next(error)
   }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -12,6 +12,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
 } from '../redux/user/userSlice'
 import { app } from '../firebase'
 const Profile = () => {
@@ -80,6 +83,25 @@ const Profile = () => {
       dispatch(updateUserFailure(error.message))
     }
   }
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart())
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      })
+      const data = await res.json()
+
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message))
+        return
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -141,7 +163,12 @@ const Profile = () => {
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span
+          className="text-red-700 cursor-pointer"
+          onClick={handleDeleteUser}
+        >
+          Delete account
+        </span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ''}</p>
