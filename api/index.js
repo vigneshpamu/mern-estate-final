632656dcf4ac1e1c017faa4df8636acea99a5083
Vignesh <vigneshpamu2002@gmail.com>
2023-09-30 20:37:32 +0530

    Complete Delete User Listing Functionality

--- api/index.js
+++ api/index.js
@@ -27,9 +27,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/listing', listingRouter)
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500
   const message = err.message || 'Internal Server Error'
+
+  console.log(message)
 
   return res.status(statusCode).json({
     success: false,
